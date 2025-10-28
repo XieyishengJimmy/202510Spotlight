c69d849c940b0e3336fedf4032c9f6dc6ef4a44c
XieyishengJimmy <thanklifetime@gmail.com>
2025-10-29 02:09:26 +0800

    新手引导功能

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
+
+    public List<Sprite> guideGroup;
+
+    public Image guideImage;
+
+    public int index = 0;
 
     private void Awake()
     {
@@ -24,11 +31,27 @@ public class LevelManager : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex);
     }
 
-
     public void ToTheMenu()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void OpenTheGuide()
+    {
+        if(index < guideGroup.Count)
+        {
+            guideImage.gameObject.SetActive(true);
+            guideImage.sprite = guideGroup[index];
+            index++;
+        }
+        else
+        {
+            guideImage.gameObject.SetActive(false);
+            index = 0;
+        }
+    }
+
+
 
 
 }
